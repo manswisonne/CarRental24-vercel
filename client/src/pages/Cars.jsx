f76bc928231b3130e8c4493fa23008e27fd8561d
Manaswi Sonne <manswisonne151@gmail.com>
2026-01-13 13:43:12 +0530

    Refactor car display to use motion.div inside map

--- client/src/pages/Cars.jsx
+++ client/src/pages/Cars.jsx
@@ -181,18 +181,18 @@ const Cars = () => {
           <>
             {/* Results */}
             {filteredCars.length > 0 ? (
-              <motion.div 
-              initial={{opacity: 0, y: 30}}
-             animate ={{opacity:1, y:0}}
-              transition={{duration: 0.4, delay:0.1*index}}
-              
-              className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 max-w-7xl mx-auto'>
-                {filteredCars.map((car, index) => (
-                  <div key={car._id || index}>
-                    <CarCard car={car} />
-                  </div>
-                ))}
-              </motion.div>
+             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 max-w-7xl mx-auto'>
+  {filteredCars.map((car, index) => (
+    <motion.div
+      key={car._id || index}
+      initial={{opacity: 0, y: 30}}
+      animate={{opacity: 1, y: 0}}
+      transition={{duration: 0.4, delay: 0.1 * index}}
+    >
+      <CarCard car={car} />
+    </motion.div>
+  ))}
+</div>
             ) : (
               <div className='text-center py-20'>
                 {input ? (
